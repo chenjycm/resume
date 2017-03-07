e1032982a28167df916740f017a163993bcb632d
chenjycm <chenjycm@163.com>
2017-03-07 16:59:11 +0800

    finish the 3rd page

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
 	}).mouseout(function(){
 		this.innerHTML="个人简历-前端工程师";
 	});
+	// 第一页下箭头动画
 	$(function(){
 		var c = document.getElementById("downCanvas"),
 	            cxt = c.getContext("2d");
@@ -32,7 +33,15 @@ $(document).ready(function() {
 	        cxt.lineJoin = "round";
 	        cxt.stroke();
 	});
-		
-
+	// 第三页柱状条动画   
+	$(document).ready(function () {
+		$('#bar-1').jqbar({ label: 'HTML/CSS', value: 82, barColor: '#D64747' });
+		$('#bar-2').jqbar({ label: 'Javascript', value: 78, barColor: '#FF681F' });
+		$('#bar-3').jqbar({ label: 'jQuery', value: 84, barColor: '#ea805c' });
+		$('#bar-4').jqbar({ label: 'React', value: 50, barColor: '#88bbc8' });
+		$('#bar-5').jqbar({ label: 'C/C++/VB', value: 60, barColor: '#939393' });
+		$('#bar-6').jqbar({ label: 'English', value: 90, barColor: '#3a89c9' });
+	});
+	
 	
 });
