58ee65aae379a62322436d6009fb8468c2d103e2
chenjycm <chenjycm@163.com>
2017-04-12 11:26:13 +0800

    fixed button click bugs

--- mobile/js/index.js
+++ mobile/js/index.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-	(function browserRedirect(){
-		  var sUserAgent = navigator.userAgent.toLowerCase();
-		  var bIsIpad = sUserAgent.match(/ipad/i) == 'ipad';
-		  var bIsIphone = sUserAgent.match(/iphone os/i) == 'iphone os';
-		  var bIsMidp = sUserAgent.match(/midp/i) == 'midp';
-		  var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == 'rv:1.2.3.4';
-		  var bIsUc = sUserAgent.match(/ucweb/i) == 'web';
-		  var bIsCE = sUserAgent.match(/windows ce/i) == 'windows ce';
-		  var bIsWM = sUserAgent.match(/windows mobile/i) == 'windows mobile';
-		  var bIsAndroid = sUserAgent.match(/android/i) == 'android';
-		  if(!(bIsIpad || bIsIphone || bIsMidp || bIsUc7 || bIsUc || bIsCE || bIsWM || bIsAndroid)){
-		  window.location.href = 'http://115.28.10.122/resume/pc/index.html';
-		  }
-	})();
-
+	// (function browserRedirect(){
+	// 	  var sUserAgent = navigator.userAgent.toLowerCase();
+	// 	  var bIsIpad = sUserAgent.match(/ipad/i) == 'ipad';
+	// 	  var bIsIphone = sUserAgent.match(/iphone os/i) == 'iphone os';
+	// 	  var bIsMidp = sUserAgent.match(/midp/i) == 'midp';
+	// 	  var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == 'rv:1.2.3.4';
+	// 	  var bIsUc = sUserAgent.match(/ucweb/i) == 'web';
+	// 	  var bIsCE = sUserAgent.match(/windows ce/i) == 'windows ce';
+	// 	  var bIsWM = sUserAgent.match(/windows mobile/i) == 'windows mobile';
+	// 	  var bIsAndroid = sUserAgent.match(/android/i) == 'android';
+	// 	  if(!(bIsIpad || bIsIphone || bIsMidp || bIsUc7 || bIsUc || bIsCE || bIsWM || bIsAndroid)){
+	// 	  window.location.href = 'http://115.28.10.122/resume/pc/index.html';
+	// 	  }
+	// })();
+	
 
 	document.documentElement.style.fontSize = window.screen.width / 3.75 + 'px';
 	$(function(){
@@ -152,4 +152,11 @@ $(document).ready(function() {
 	        // cxt.lineJoin = "round";
 	        // cxt.stroke();
 	});
+	
+	 $("#b5-2-button").click(function(event) {
+        event.stopPropagation();
+	 	window.open('http://115.28.10.122/resume/mobile');
+		console.log('haha');
+      });
 });
+
